5c827427183f2a2f0da7102e6b9be1c3e7b3279f
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2024-04-02 11:47:54 -0300

    aula 0204

--- basic/basictools/src/components/Form/Form.jsx
+++ basic/basictools/src/components/Form/Form.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-var teste=8;
+import React, { useState } from 'react'
+var teste = 8
 const Form = () => {
     const handleMyEvent = (e) => {
         console.log(e)
@@ -12,34 +13,34 @@ const Form = () => {
             return <h1>Renderizou lindinho!</h1>
         }
     }
+    const [x, setX] = useState(false)
     return (
         <>
             <div>
                 <div>
-                    <button onClick={handleMyEvent}>Click aqui</button>
+                    <button onClick={handleMyEvent}>Clique Aqui</button>
                 </div>
                 <div>
-                    <button onClick={() => { console.log("Yesss!! clicou !!!") }}>Clicka em mim tambem !</button>
+                    <button onClick={() => { console.log("Yes! Apertou em mim.") }}>Clique em mim também!</button>
                 </div>
                 <div>
                     <button onClick={() => {
                         if (true) {
-                            console.log("Isso não deveria acontecer")
+                            console.log("Isso não deveria acontecer!")
                         }
-                    }}>Será que vc clicaria aqui ?
-                    </button>
+                    }}>Será que você clicaria aqui?</button>
                 </div>
                 <div>
                     {renderButton(true)}
                     {renderButton(false)}
+                    <h1>{x ? 'Renderizou' : 'Deu ruim não foi'}</h1>
+                    <button onClick={() => setX(!x)}>Valida pepino</button>
                 </div>
-
             </div>
             <div>
-                <strong>Valor: </strong> {teste}
-                <button onClick={()=>{
-                    teste = 12;console.log(teste)}}>Aperte e mude</button>
-                <strong>Valor: </strong>{teste}
+                <strong>Valor:</strong>{teste}
+                <button onClick={() => { teste = 12; console.log(teste) }}>Aperte e mude</button>
+                <strong>Valor:</strong>{teste}
             </div>
         </>
     )
