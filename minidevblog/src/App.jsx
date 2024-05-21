e8952b11da6b04abab5a14fa5693ce5b2c67381f
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2024-05-21 12:28:13 -0300

    aula2105

--- minidevblog/src/App.jsx
+++ minidevblog/src/App.jsx
@@ -1,13 +1,46 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate, Form } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { onAuthStateChanged, useDeviceLanguage } from 'firebase/auth'
+import { useState, useEffect } from 'react'
+import { userAuthentication } from './hooks/userAuthentication'
+import loading from './assets/Loading.gif'
 
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+
 
 function App() {
+  const [user, setUser] = useState(undefined)
+  const { auth } = userAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      setUser(user)
+    })
+  }, [auth])
+  if (loadingUser) {
+    return <div className='container load'><img rc={loading} alt='gif Loading Imgage' width='120px' height='120px' /></div>
+  }
   return (
     <>
-          <Navbar />
-          <Footer />
+      <AuthProvider value={{ user }} />
+      <BrowserRouter>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/' element={<Home />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+      <AuthProvider />
     </>
   )
 }
